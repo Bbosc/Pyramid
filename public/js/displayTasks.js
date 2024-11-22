868b8839520d1949a2fcced61cc333827457339c
Baudouin Bosc <baudouin.bosc@gmail.com>
2024-11-22 18:32:01 +0100

    css basis

--- public/js/displayTasks.js
+++ public/js/displayTasks.js
@@ -1,7 +1,9 @@
 function displayTaskPerTiers(tasks, tier) {
-    const container = document.getElementById("tasks-overview");
-    const section = document.createElement("h2");
-    section.textContent = "Tier " + tier;
+    const container = document.getElementById("tasks-container");
+    const div = document.createElement("div");
+    div.className = "column";
+    const title = document.createElement("h2");
+    title.textContent = "Tier " + tier;
     
     const ul = document.createElement("ul");
 
@@ -11,24 +13,31 @@ function displayTaskPerTiers(tasks, tier) {
         }
     ).forEach(task => {
         const li = document.createElement("li");
+        li.className = "task";
         li.textContent = task.name;
 
         const editForm = document.createElement("form");
         editForm.action = "/tasks/edit/" + task.name;
         editForm.method =  "GET";
         const editButton = document.createElement("button");
+        editButton.className = "btn";
         editButton.name = "name";
         editButton.value = task.name;
-        editButton.textContent = "Edit";
+        const editIcon = document.createElement("i");
+        editIcon.className = "fa fa-edit";
+        editButton.appendChild(editIcon);
         editForm.appendChild(editButton);
 
         const deleteForm = document.createElement("form");
         deleteForm.action = "/tasks/delete/" + task.name;
         deleteForm.method =  "POST";
         const deleteButton = document.createElement("button");
+        deleteButton.className = "btn";
         deleteButton.name = "name";
         deleteButton.value = task.name;
-        deleteButton.textContent = "Remove";
+        const deleteIcon = document.createElement("i");
+        deleteIcon.className = "fa fa-trash";
+        deleteButton.appendChild(deleteIcon);
         deleteForm.appendChild(deleteButton);
 
         li.appendChild(editForm);
@@ -36,6 +45,7 @@ function displayTaskPerTiers(tasks, tier) {
 
         ul.appendChild(li);
     })
-    container.parentNode.insertBefore(section, container.nextSibling);
-    section.parentNode.insertBefore(ul, section.nextSibling);
+    div.appendChild(title);
+    div.appendChild(ul);
+    container.appendChild(div);
 }
